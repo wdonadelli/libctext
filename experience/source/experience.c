3510858043f02d450b11e7ebee21b0922feda254
wd <wdonadelli@gmail.com>
2021-03-13 01:41:24 -0300

    Iniciando os trabalhos

--- experience/source/experience.c
+++ experience/source/experience.c
@@ -4,9 +4,9 @@
 #define STYLE_RESET   "\e[0m"         /* limpa a formatação */
 #define STYLE_DEFAULT "\e[0;0;37;44m" /* estilo principal */
 #define STYLE_TITLE   "\e[0;1;34;47m" /* estilo do título */
-#define STYLE_METHOD  "\e[0;0;37;44m" /* estilo da aplicaçãop */
+#define STYLE_METHOD  "\e[0;2;37;44m" /* estilo da aplicaçãop */
 #define STYLE_VALUE   "\e[0;3;33;44m" /* estilo do valor */
-#define STYLE_PROMPT  "\e[0;0;33;44m" /* estilo do questionamento */
+#define STYLE_PROMPT  "\e[0;0;32;44m" /* estilo do questionamento */
 
 /*-- Frases --*/
 #define INPUT_INITIAL STYLE_PROMPT "Initial value: " STYLE_VALUE
@@ -20,13 +20,59 @@
 /*-- Complementos --*/
 #define MAIN "myString"
 
-#define QUOTES(VALUE)\
-	STYLE_VALUE "\"" #VALUE "\"" STYLE_DEFAULT
+#define VALUE(RETURN, TYPE) \
+	printf( \
+		( \
+			TYPE == 's' ? "%s\"%s\"" : ( \
+			TYPE == 'd' ? "%s%d" : ( \
+			TYPE == 'l' ? "\n%s%ld" : \
+			"%s%s" )) \
+		), \
+		STYLE_VALUE, RETURN \
+	)
 
-#define METHOD(NAME)\
-	STYLE_METHOD NAME() STYLE_DEFAULT
 
-#define METHOD(NAME, ATTR) STYLE_METHOD #MAIN.#NAME#(QUOTES(ATTR)STYLE_METHOD); STYLE_DEFAULT
+#define METHOD(NAME) \
+	printf("%s%s.%s();\n", STYLE_METHOD, MAIN, NAME )
+
+#define METHODI(NAME, ATTR) \
+	printf("%s%s.%s(%s\"%s\"%s);\n", \
+		STYLE_METHOD, MAIN, NAME, \
+		STYLE_VALUE, ATTR, STYLE_METHOD \
+	)
+
+#define METHOD0(NAME, RETURN, TYPE) \
+	printf( \
+		( \
+			TYPE == 's' ? "%s%s.%s();\n%s\"%s\"%s" : ( \
+			TYPE == 'd' ? "%s%s.%s();\n%s%d%s" : ( \
+			TYPE == 'l' ? "%s%s.%s();\n%s%ld%s" : \
+			              "%s%s.%s();\n%s%s%s" )) \
+		), \
+		STYLE_METHOD, MAIN, NAME, STYLE_VALUE, RETURN, STYLE_DEFAULT\
+	)
+	
+#define METHOD1(NAME, ATTR, RETURN, TYPE) \
+	printf( \
+		( \
+			TYPE == 's' ? "%s%s.%s(%s\"%s\"%s);\n%s\"%s\"%s" : ( \
+			TYPE == 'd' ? "%s%s.%s(%s\"%s\"%s);\n%s%d%s" : ( \
+			TYPE == 'l' ? "%s%s.%s(%s\"%s\"%s);\n%s%ld%s" : \
+			              "%s%s.%s(%s\"%s\"%s);\n%s%s%s" )) \
+		), \
+		STYLE_METHOD, MAIN, NAME, \
+		STYLE_VALUE, ATTR, STYLE_METHOD, \
+		STYLE_VALUE, RETURN, STYLE_DEFAULT \
+	)
+
+#define METHOD2(NAME, ATTR1, ATTR2, RETURN) \
+	printf("%s%s.%s(%s\"%s\"%s, %s\"%s\"%s);\n%s\"%s\"", \
+		STYLE_METHOD, MAIN, NAME, \
+		STYLE_VALUE, ATTR1, STYLE_METHOD, \
+		STYLE_VALUE, ATTR2, STYLE_METHOD, \
+		STYLE_VALUE, RETURN \
+	)
+
 
 /*----------------------------------------------------------------------------*/
 int main(int argc, char *argv[]) {
@@ -65,116 +111,48 @@ int main(int argc, char *argv[]) {
 			printf("%smyString.free();", STYLE_METHOD);
 			quit = 1;
 		} else if (data.match("get")) {
-			printf("%s\n%s",
-				METHOD("get"), QUOTES(myString.get())
-			);
+			METHOD0("get", myString.get(), 's');
 		} else if (data.match("lower")) {
-			printf("%smyString.lower();\n%s\"%s\"",
-				STYLE_METHOD, STYLE_VALUE, myString.lower()
-			);
+			METHOD0("lower", myString.lower(), 's');
 		} else if (data.match("upper")) {
-			printf("%smyString.upper();\n%s\"%s\"",
-				STYLE_METHOD, STYLE_VALUE, myString.upper()
-			);
+			METHOD0("upper", myString.upper(), 's');
 		} else if (data.match("title")) {
-			printf("%smyString.title();\n%s\"%s\"",
-				STYLE_METHOD, STYLE_VALUE, myString.title()
-			);
+			METHOD0("title", myString.title(), 's');
 		} else if (data.match("trim")) {
-			printf("%smyString.trim();\n%s\"%s\"",
-				STYLE_METHOD, STYLE_VALUE, myString.trim()
-			);
+			METHOD0("trim", myString.trim(), 's');
 		} else if (data.match("rtrim")) {
-			printf("%smyString.rtrim();\n%s\"%s\"",
-				STYLE_METHOD, STYLE_VALUE, myString.rtrim()
-			);
+			METHOD0("rtrim", myString.rtrim(), 's');
 		} else if (data.match("ltrim")) {
-			printf("%smyString.ltrim();\n%s\"%s\"",
-				STYLE_METHOD, STYLE_VALUE, myString.ltrim()
-			);
+			METHOD0("ltrim", myString.ltrim(), 's');
 		} else if (data.match("clear")) {
-			printf("%smyString.clear();\n%s\"%s\"",
-				STYLE_METHOD, STYLE_VALUE, myString.clear()
-			);
+			METHOD0("clear", myString.clear(), 's');
 		} else if (data.match("len")) {
-			printf("%smyString.len();\n%s%ld ",
-				STYLE_METHOD, STYLE_VALUE, myString.len()
-			);
+			METHOD0("len", myString.len(), 'l');
 		} else if (data.match("write")) {
-			printf("%smyString.write();\n", STYLE_METHOD);
-			printf("%s%d", STYLE_VALUE, myString.write());
+			METHOD0("write", "", 'x');
+			VALUE(myString.write(), 'd');
 		} else if (data.match("set")) {
 			data.read(INPUT_SET);
-			printf("%smyString.set(%s\"%s\"%s);\n%s\"%s\"",
-				STYLE_METHOD, 
-				STYLE_VALUE,
-				data.get(),
-				STYLE_METHOD,
-				STYLE_VALUE,
-				myString.set(data.get())
-			);
+			METHOD1("set", data.get(), myString.set(data.get()), 's');
 		} else if (data.match("add")) {
 			data.read(INPUT_ADD);
-			printf("%smyString.add(%s\"%s\"%s);\n%s\"%s\"",
-				STYLE_METHOD, 
-				STYLE_VALUE,
-				data.get(),
-				STYLE_METHOD,
-				STYLE_VALUE,
-				myString.add(data.get())
-			);
+			METHOD1("add", data.get(), myString.add(data.get()), 's');
 		} else if (data.match("read")) {
-			int check;
 			data.read(INPUT_INPUT);
-			printf("%smyString.read(%s\"%s\"%s);\n%s",
-				STYLE_METHOD, 
-				STYLE_VALUE,
-				data.get(),
-				STYLE_METHOD,
-				STYLE_DEFAULT
-			);
-			check = myString.read(data.get());
-			printf("%s%d", STYLE_VALUE, check);
+			printf(STYLE_DEFAULT);
+			METHOD1("read", data.get(), myString.read(data.get()), 'd');
 		} else if (data.match("fwrite")) {
 			data.read(INPUT_FILE);
-			printf("%smyString.fwrite(%s\"%s\"%s);\n%s%d",
-				STYLE_METHOD, 
-				STYLE_VALUE,
-				data.get(),
-				STYLE_METHOD,
-				STYLE_VALUE,
-				myString.fwrite(data.get())
-			);
+			METHOD1("fwrite", data.get(), myString.fwrite(data.get()), 'd');
 		} else if (data.match("fread")) {
 			data.read(INPUT_FILE);
-			printf("%smyString.fread(%s\"%s\"%s);\n%s%d",
-				STYLE_METHOD, 
-				STYLE_VALUE,
-				data.get(),
-				STYLE_METHOD,
-				STYLE_VALUE,
-				myString.fread(data.get())
-			);
+			METHOD1("fread", data.get(), myString.fread(data.get()), 'd');
 		} else if (data.match("match")) {
 			data.read(INPUT_CHECK);
-			printf("%smyString.match(%s\"%s\"%s);\n%s%d",
-				STYLE_METHOD, 
-				STYLE_VALUE,
-				data.get(),
-				STYLE_METHOD,
-				STYLE_VALUE,
-				myString.match(data.get())
-			);
+			METHOD1("match", data.get(), myString.match(data.get()), 'd');
 		} else if (data.match("index")) {
 			data.read(INPUT_CHECK);
-			printf("%smyString.index(%s\"%s\"%s);\n%s%ld",
-				STYLE_METHOD, 
-				STYLE_VALUE,
-				data.get(),
-				STYLE_METHOD,
-				STYLE_VALUE,
-				myString.index(data.get())
-			);
+			METHOD1("index", data.get(), myString.index(data.get()), 'l');
 		} else if (data.match("replace")) {
 			data.read(INPUT_CHECK);
 			attr.read(INPUT_CHECK);
@@ -241,7 +219,7 @@ int main(int argc, char *argv[]) {
 	/* sair */
 	data.free();
 	attr.free();
-	printf("\n\nPress enter to exit...");
+	printf("\a%s\n\nPress enter to exit...", STYLE_DEFAULT);
 	fgetc(stdin);
 	puts(STYLE_RESET);
 	system("clear");
